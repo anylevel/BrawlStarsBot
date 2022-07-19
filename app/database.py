a9c921835d27f7356035aebd82a28a7286582e97
anylevel <vadyusha.surin@gmail.com>
2022-07-19 21:56:39 +0300

    add new file

---
+++ app/database.py
@@ -0,0 +1,8 @@
+import aiosqlite
+
+
+async def create_table() -> None:
+    async with aiosqlite.connect("stats.db") as db:
+        await db.execute(
+            '''CREATE TABLE IF NOT EXISTS users (id integer primary key , user_name text , game_hashtag text)''')
+        await db.commit()
