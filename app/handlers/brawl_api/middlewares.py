d1b166d2ba96f0f89c44a3988bdda8af4b83174e
anylevel <vadyusha.surin@gmail.com>
2022-08-04 02:17:20 +0300

    add middleware for brawl api commands

---
+++ app/handlers/brawl_api/middlewares.py
@@ -0,0 +1,25 @@
+from app.models import User
+from aiogram.dispatcher.handler import CancelHandler, current_handler
+from aiogram.dispatcher.middlewares import BaseMiddleware
+from aiogram import types
+from pathlib import Path
+
+class SomeMiddleware(BaseMiddleware):
+    def __init__(self):
+        self.flag = False
+        super(SomeMiddleware, self).__init__()
+
+    async def on_pre_process_message(self, message: types.Message, data: dict):
+        handler = current_handler.get()
+        module_name = handler.__module__.split('.')[-1]
+        if handler and module_name == "brawl_api":
+            await self.check_token(message)
+
+    async def check_token(self, message: types.Message):
+        user = await User.filter(name=message.from_user.username).first()
+        if not user:
+            await message.answer("Произошло что-то непредвиденное, пожалуйста запустите команду /start")
+            raise CancelHandler()
+        elif user.token is None:
+            await message.answer(f"Токен отсутствует, пожалуйста запустите команду /start")
+            raise CancelHandler()
