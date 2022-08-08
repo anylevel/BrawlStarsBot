166aeb0973ad815bb7265d132c5a29affae1eab0
anylevel <vadyusha.surin@gmail.com>
2022-08-08 12:02:17 +0300

    TODO finish refactoring operetions orm

--- app/handlers/common/common.py
+++ app/handlers/common/common.py
@@ -4,19 +4,23 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from app.models import User
 
-#TODO Написать валидатор для хештега, понять как доставать атрибуты из tortoise orm
-#TODO разобраться с middleware
+
+# TODO Написать валидатор для хештега
+# TODO разобраться с middleware
 
 
 class Token(StatesGroup):
     waiting_for_get_token = State()
 
+    def __str__(self):
+        return "TokenState"
+
 
 @dp.message_handler(commands=["start"], state="*")
 async def start(message: types.Message, state: FSMContext):
-    user = await User.filter(name=message.from_user.username).first()
+    user = await User.get_or_none(name=message.from_user.username)
     if user:
-        if user.token is not None:
+        if user.token:
             await message.answer(f"Вы уже отправили токен боту,чтобы его поменять, воспользуйтесь командой /change")
             await state.finish()
             return
@@ -26,8 +30,8 @@ async def start(message: types.Message, state: FSMContext):
 
 @dp.message_handler(commands=["change"], state='*')
 async def change(message: types.Message, state: FSMContext):
-    user = await User.filter(name=message.from_user.username).first()
-    if not user:
+    user = await User.get_or_none(name=message.from_user.username)
+    if user is None:
         await message.answer("Произошло что-то непредвиденное, пожалуйста запустите команду /start")
         await state.finish()
         return
@@ -47,11 +51,12 @@ async def cancel(message: types.Message, state: FSMContext):
 
 @dp.message_handler(state=Token.waiting_for_get_token)
 async def finish_token(message: types.Message, state: FSMContext):
-    print(message.text)
-    user = User.filter(name=message.from_user.username).first()
-    if not user:
+    user = await User.get_or_none(name=message.from_user.username)
+    if user is None:
         await User.create(name=message.from_user.username, token=message.text)
+        await state.finish()
         return
     user.token = message.text
     await user.save()
     await state.finish()
+    await message.reply("Токен успешно обновлен!")
