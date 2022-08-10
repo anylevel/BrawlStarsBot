f7ce0719714a1e561fde4ff0a716f0dd76730c78
anylevel <vadyusha.surin@gmail.com>
2022-08-10 13:06:00 +0300

    changed

--- app/constans.py
+++ app/constans.py
@@ -1,2 +1,12 @@
+import aiofiles
+
+
+async def get_token() -> str:
+    async with aiofiles.open("config.txt", 'r') as file:
+        data = await file.read()
+    token = data.rstrip()
+    return token
+
+
 commands = ["/start", "/change", "/cancel", "/player_info"]
 states = ["Token:waiting_for_get_token"]
