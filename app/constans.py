47f9a012b773fbaa7ab919a24bd9a9967b384739
anylevel <vadyusha.surin@gmail.com>
2022-08-10 17:42:39 +0300

    rename method add method get token bot

--- app/constans.py
+++ app/constans.py
@@ -1,11 +1,19 @@
 import aiofiles
+import os
 
 
-async def get_token() -> str:
+async def get_token_api() -> str:
     async with aiofiles.open("config.txt", 'r') as file:
         data = await file.read()
     token = data.rstrip()
     return token
+
+
+async def get_token_bot() -> str:
+    bot_token = os.getenv("API_TOKEN")
+    if not bot_token:
+        raise ValueError("Ошибка при получении токена")
+    return bot_token
 
 
 commands = ["/start", "/change", "/cancel", "/player_info"]
