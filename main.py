fcbb7e881a470c148e1df77389b759145430a59f
anylevel <vadyusha.surin@gmail.com>
2022-08-10 17:44:02 +0300

    токен теперь берется из метода

--- main.py
+++ main.py
@@ -4,22 +4,17 @@ from aiohttp import ClientSession
 from app.sessions import Sessions
 from app.database import create_table, init
 from app.middlewares import ThrottlingMiddleware, HandlerMiddleware
-from app.constans import get_token
+from app.constans import get_token_api, get_token_bot
 import asyncio
-import os
 
-BOT_TOKEN = os.getenv("API_TOKEN")
-if not BOT_TOKEN:
-    print("BOT_TOKEN is not found")
-    exit(1)
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=await get_token_bot())
 dp = Dispatcher(bot=bot, storage=MemoryStorage())
 
 
 async def main():
     headers = {
         "Accept": "application/json",
-        "authorization": f"Bearer {await get_token()}"
+        "authorization": f"Bearer {await get_token_api()}"
     }
     async with ClientSession(headers=headers) as session:
         from app.handlers import dp, TokenMiddleware
