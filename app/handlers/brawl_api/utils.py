71b1e6aae5d97e9d0e631192d06e80d133866d35
anylevel <vadyusha.surin@gmail.com>
2022-08-12 23:54:04 +0300

    add hashtag_clan_check function

--- app/handlers/brawl_api/utils.py
+++ app/handlers/brawl_api/utils.py
@@ -15,6 +15,16 @@ async def hashtag_check(hashtag: str) -> tuple[str, bool]:
     return hashtag, True
 
 
+async def hashtag_clan_check(hashtag: str) -> tuple[str, bool]:
+    hashtag = hashtag.upper()
+    url = f"https://api.brawlstars.com/v1/clubs/%23{hashtag}/"
+    async with Sessions.get_response(name=session_name, url=url) as response:
+        data = await response.json()
+    if 'reason' in data:
+        return hashtag, False
+    return hashtag, True
+
+
 async def get_token(message: types.Message):
     user = await User.get(name=message.from_user.username)
     return user.token
