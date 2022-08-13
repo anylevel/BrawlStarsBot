a302a7e54f8e9db93ad0569e955dfd16768ec207
anylevel <vadyusha.surin@gmail.com>
2022-08-13 11:03:02 +0300

    player_info sends a brawler sticker whish is top 1

--- app/handlers/brawl_api/brawl_api.py
+++ app/handlers/brawl_api/brawl_api.py
@@ -2,6 +2,7 @@ from aiogram import types
 from main import dp
 from app.sessions import Sessions
 from .utils import session_name, get_token
+from app.constans import brawlers_dict
 
 
 @dp.message_handler(commands=["player_info"])
@@ -13,13 +14,15 @@ async def get_player_info(message: types.Message):
             Sessions.get_response(name=session_name, url=url_get_brawlers) as response_information_brawlers:
         brawlers = await response_information_brawlers.json()
         data = await response_information_player.json()
-        top_brawlers = sorted(data['brawlers'], key=lambda item: (item["highestTrophies"], item["trophies"]),
-                              reverse=True)[:5]
-        # TODO применить метод к именом бравлеров чтобы началось с большой буквы а потом прописные
+        top_brawlers = sorted(data['brawlers'], key=lambda item: item["trophies"], reverse=True)[:5]
         await message.answer(f"Name:{data['name']}\n"
                              f"Trophies:{data['trophies']}\n"
                              f"Highest Trophies: {data['highestTrophies']}\n"
                              f"{len(data['brawlers'])}/{len(brawlers['items'])} Brawlers\n"
+                             f"Victories:\n"
+                             f"3vs3:{data['3vs3Victories']}\n"
+                             f"solo:{data['soloVictories']}\n"
+                             f"duo:{data['duoVictories']}\n"
                              f"Club: {data['club']['name']}\n\n"
                              f"Top 5 brawlers by trophies:\n"
                              f"  1.{top_brawlers[0]['name'].capitalize()}\n"
@@ -28,6 +31,7 @@ async def get_player_info(message: types.Message):
                              f"  4.{top_brawlers[3]['name'].capitalize()}\n"
                              f"  5.{top_brawlers[4]['name'].capitalize()}\n"
                              )
+        await message.answer_sticker(brawlers_dict[top_brawlers[0]['name']])
 
 # TODO кланы выводить название открыт/закрыт сколько трофеев нужно сколько всего трофеев
 # TODO профиль президента сколько членов старейшин и т.д и топ 5 игроков
