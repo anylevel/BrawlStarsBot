41514b9e56d3feabb35972017bf74b927fe0121b
anylevel <vadyusha.surin@gmail.com>
2022-08-13 12:25:13 +0300

    command clan_info realesed + refactoring player_info command

--- app/handlers/brawl_api/brawl_api.py
+++ app/handlers/brawl_api/brawl_api.py
@@ -1,7 +1,8 @@
+from typing import Dict, List, Tuple
 from aiogram import types
 from main import dp
 from app.sessions import Sessions
-from .utils import session_name, get_token
+from .utils import session_name, get_token, get_clan_token
 from app.constans import brawlers_dict
 
 
@@ -14,25 +15,71 @@ async def get_player_info(message: types.Message):
             Sessions.get_response(name=session_name, url=url_get_brawlers) as response_information_brawlers:
         brawlers = await response_information_brawlers.json()
         data = await response_information_player.json()
-        top_brawlers = sorted(data['brawlers'], key=lambda item: item["trophies"], reverse=True)[:5]
-        await message.answer(f"Name:{data['name']}\n"
-                             f"Trophies:{data['trophies']}\n"
-                             f"Highest Trophies: {data['highestTrophies']}\n"
-                             f"{len(data['brawlers'])}/{len(brawlers['items'])} Brawlers\n"
-                             f"Victories:\n"
-                             f"3vs3:{data['3vs3Victories']}\n"
-                             f"solo:{data['soloVictories']}\n"
-                             f"duo:{data['duoVictories']}\n"
-                             f"Club: {data['club']['name']}\n\n"
-                             f"Top 5 brawlers by trophies:\n"
-                             f"  1.{top_brawlers[0]['name'].capitalize()}\n"
-                             f"  2.{top_brawlers[1]['name'].capitalize()}\n"
-                             f"  3.{top_brawlers[2]['name'].capitalize()}\n"
-                             f"  4.{top_brawlers[3]['name'].capitalize()}\n"
-                             f"  5.{top_brawlers[4]['name'].capitalize()}\n"
-                             )
-        await message.answer_sticker(brawlers_dict[top_brawlers[0]['name']])
+    top_brawlers = sorted(data['brawlers'], key=lambda item: item["trophies"], reverse=True)[:5]
+    text_brawlers = ''
+    for count, top_brawler in enumerate(top_brawlers, start=1):
+        text_brawlers += f"  {count}.{top_brawler['name'].capitalize()}\n"
+    await message.answer(f"Name:{data['name']}\n"
+                         f"Trophies:{data['trophies']}\n"
+                         f"Highest Trophies: {data['highestTrophies']}\n"
+                         f"{len(data['brawlers'])}/{len(brawlers['items'])} Brawlers\n"
+                         f"Victories:\n"
+                         f"3vs3:{data['3vs3Victories']}\n"
+                         f"solo:{data['soloVictories']}\n"
+                         f"duo:{data['duoVictories']}\n"
+                         f"Club: {data['club']['name']}\n\n"
+                         f"Top 5 brawlers by trophies:\n"
+                         f"{text_brawlers}"
+                         )
+    await message.answer_sticker(brawlers_dict[top_brawlers[0]['name']])
+
 
 # TODO кланы выводить название открыт/закрыт сколько трофеев нужно сколько всего трофеев
 # TODO профиль президента сколько членов старейшин и т.д и топ 5 игроков
-# @dp.message_handler(commands=[""])
+
+async def info_clan_members(members: List) -> Tuple[Dict, Dict]:
+    president = dict()
+    members_role = {"vicePresident": 0, "senior": 0, "member": 0}
+    for member in members:
+        if member["role"] == "president":
+            president = member
+        if member["role"] == "vicePresident":
+            members_role["vicePresident"] += 1
+        elif member["role"] == "senior":
+            members_role["senior"] += 1
+        elif member["role"] == "member":
+            members_role["member"] += 1
+    return president, members_role
+
+
+@dp.message_handler(commands=["clan_info"])
+async def get_clan_info(message: types.Message):
+    clan_token = await get_clan_token(message=message)
+    url_get_clan_info = f"https://api.brawlstars.com/v1/clubs/%23{clan_token}/"
+    async with Sessions.get_response(name=session_name, url=url_get_clan_info) as response_clan_info:
+        data = await response_clan_info.json()
+    president, members_role = await info_clan_members(members=data["members"])
+    top_players = data["members"][:5]
+    await message.answer(f"Clan name:{data['name']}\n"
+                         f"type:{data['type']}\n"
+                         f"Required trophies:{data['requiredTrophies']}\n"
+                         f"Trophies:{data['trophies']}\n"
+                         f"Members:{len(data['members'])}/30\n"
+                         f"President:\n"
+                         f"     Name:{president['name']}\n"
+                         f"     Tag:{president['tag']}\n"
+                         f"     Trophies:{president['trophies']}\n"
+                         f"Vice Presidents:{members_role['vicePresident']}\n"
+                         f"Seniors:{members_role['senior']}\n"
+                         f"Members:{members_role['member']}\n")
+    await message.answer_sticker(r'CAACAgIAAxkBAAEFjJJi92Whb6i8h07EfMKTQGqJCAPpRgACdA4AAh7GUEviJXY_KNTeLykE')
+    text_players = ''
+    for count, top_player in enumerate(top_players, start=1):
+        text_players += f"""
+        {count}.Name:{top_player['name']}
+                Tag:{top_player['tag']}
+                Trophies{top_player['trophies']}                        
+                        """
+
+    await message.answer(f"Top 5 players of the clan:\n"
+                         f"{text_players}")
