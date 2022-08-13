b87fc133373283a8d0fa01793a8c752af6272a14
anylevel <vadyusha.surin@gmail.com>
2022-08-13 03:24:08 +0300

    commited

--- app/constans.py
+++ app/constans.py
@@ -9,12 +9,12 @@ async def get_token_api() -> str:
     return token
 
 
-async def get_token_bot() -> str:
+def get_token_bot() -> str:
     bot_token = os.getenv("API_TOKEN")
     if not bot_token:
         raise ValueError("Ошибка при получении токена")
     return bot_token
 
 
-commands = ["/start", "/change", "/cancel", "/player_info"]
-states = ["Token:waiting_for_get_token"]
+#commands = ["/start", "/change", "/cancel", "/player_info"]
+#states = ["Token:waiting_for_get_token", "ClanToken:waiting_for_get_token", "ClanToken:finish_get_token"]
