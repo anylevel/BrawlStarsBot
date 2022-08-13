0a79da6560a6804c1829ee500a356a1b98f82d92
anylevel <vadyusha.surin@gmail.com>
2022-08-13 03:24:32 +0300

    add attr clan token

--- app/models.py
+++ app/models.py
@@ -5,7 +5,8 @@ from tortoise import fields
 class User(Model):
     id = fields.IntField(pk=True)
     name = fields.CharField(max_length=100)
-    token = fields.CharField(max_length=10, default=None)
+    token = fields.CharField(max_length=9, default=None)
+    clan_token = fields.CharField(max_length=9, null=True)
 
     def __str__(self):
-        return f'{self.name}:{self.token}'
+        return f'{self.name}:{self.token}:{self.clan_token}'
