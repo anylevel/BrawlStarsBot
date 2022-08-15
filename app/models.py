b7045a56f79756e4b2aaab542510971c777b820e
anylevel <vadyusha.surin@gmail.com>
2022-08-15 22:53:27 +0300

    add model player + foreign key in user

--- app/models.py
+++ app/models.py
@@ -7,9 +7,20 @@ class User(Model):
     name = fields.CharField(max_length=100)
     token = fields.CharField(max_length=9, default=None)
     clan_token = fields.CharField(max_length=9, null=True)
+    player = fields.ForeignKeyField('models.Player', related_name='player')
 
     def __str__(self):
         return f'{self.name}:{self.token}:{self.clan_token}'
 
-#TODO сделать еще одну модель типа токен-battlelog и сделать foreign key! разобраться с ним!
-#TODO подумать нужно ли добавлять поля типа процент выйграша проигрыш и тд
+
+class Player(Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=100, null=True)
+    token = fields.CharField(max_length=9, default=None)
+    trophies = fields.IntField(null=True)
+    battle_log = fields.data.JSONField(null=True)
+
+    # def __str__(self):
+    #     return f'{self.name}:{self.trophies}'
+# TODO сделать еще одну модель типа токен-battlelog и сделать foreign key! разобраться с ним!
+# TODO подумать нужно ли добавлять поля типа процент выйграша проигрыш и тд
