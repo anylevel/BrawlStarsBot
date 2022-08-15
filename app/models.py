79b6441b6820fdd7f4d7d13131bf01a0e574b9b6
anylevel <vadyusha.surin@gmail.com>
2022-08-16 02:00:33 +0300

    add model clan + foreign key clan in user model

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ class User(Model):
     token = fields.CharField(max_length=9, default=None)
     clan_token = fields.CharField(max_length=9, null=True)
     player = fields.ForeignKeyField('models.Player', related_name='player')
+    clan = fields.ForeignKeyField('models.Player', related_name='clan')
 
     def __str__(self):
         return f'{self.name}:{self.token}:{self.clan_token}'
@@ -15,10 +16,10 @@ class User(Model):
 
 class Player(Model):
     id = fields.IntField(pk=True)
-    name = fields.CharField(max_length=100, null=True)
-    token = fields.CharField(max_length=9, default=None)
-    trophies = fields.IntField(null=True)
-    highest_trophies = fields.IntField(null=True)
+    name = fields.CharField(max_length=50)
+    token = fields.CharField(max_length=9)
+    trophies = fields.IntField()
+    highest_trophies = fields.IntField()
     battle_log = fields.data.JSONField(null=True)
 
     class Meta:
@@ -26,5 +27,16 @@ class Player(Model):
 
     def __str__(self):
         return f'Name:{self.name}\n\t\t  Token:#{self.token}\n\t\t  Trophies:{self.trophies}\n'
+
+
+class Clan(Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=50)
+    token = fields.CharField(max_length=9)
+    trophies = fields.IntField(null=True)
+
+    def __str__(self):
+        return f'Name:{self.name}\n\t\t  Token:#{self.token}\n\t\t  Trophies:{self.trophies}\n'
+
 # TODO сделать еще одну модель типа токен-battlelog и сделать foreign key! разобраться с ним!
 # TODO подумать нужно ли добавлять поля типа процент выйграша проигрыш и тд
