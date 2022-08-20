673e5e91ba47cb69274be235f632963db5a26039
anylevel <vadyusha.surin@gmail.com>
2022-08-20 23:31:09 +0300

    add constant name session brawlify

--- app/handlers/brawl_api/utils.py
+++ app/handlers/brawl_api/utils.py
@@ -3,23 +3,25 @@ from app.models import User, Player, Clan
 from aiogram import types
 from typing import Any
 
-session_name = "brawl_api"
+session_name_brawl_api = "brawl_api"
+session_name_brawlify = "brawlify"
 
 
 async def hashtag_check(hashtag: str) -> tuple[str, bool, Any]:
     hashtag = hashtag.upper()
     url = f"https://api.brawlstars.com/v1/players/%23{hashtag}/"
-    async with Sessions.get_response(name=session_name, url=url) as response:
+    async with Sessions.get_response(name=session_name_brawl_api, url=url) as response:
         data = await response.json()
     if 'reason' in data:
         return hashtag, False, None
     return hashtag, True, data
 
 
+# TODO сделать чтобы кушал хештег с решеткой
 async def hashtag_clan_check(hashtag: str) -> tuple[str, bool, Any]:
     hashtag = hashtag.upper()
     url = f"https://api.brawlstars.com/v1/clubs/%23{hashtag}/"
-    async with Sessions.get_response(name=session_name, url=url) as response:
+    async with Sessions.get_response(name=session_name_brawl_api, url=url) as response:
         data = await response.json()
     if 'reason' in data:
         return hashtag, False, None
