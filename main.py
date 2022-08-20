24d0610f96d0c89e3f8f61701c35ef9043655aeb
anylevel <vadyusha.surin@gmail.com>
2022-08-20 23:29:23 +0300

    new session brawlify headers now in constants

--- main.py
+++ main.py
@@ -4,26 +4,24 @@ from aiohttp import ClientSession
 from app.sessions import Sessions
 from app.database import create_table, init
 from app.middlewares import ThrottlingMiddleware, TokenMiddleware
-from app.constans import get_token_api, get_token_bot
+from app.constans import get_token_bot, headers_brawl_api, headers_brawlify
 import asyncio
 
 bot = Bot(token=get_token_bot())
 dp = Dispatcher(bot=bot, storage=MemoryStorage())
 
 
-#TODO Разобраться в локалях языка и добавить их
-#TODO Добавить логирование aiologger/logger/loguru?
-#TODO добавить readme.md и в репу добавить пример config файла!
+# TODO Разобраться в локалях языка и добавить их
+# TODO Добавить логирование aiologger/logger/loguru?
+# TODO добавить readme.md и в репу добавить пример config файла!
 
 
 async def main():
-    headers = {
-        "Accept": "application/json",
-        "authorization": f"Bearer {await get_token_api()}"
-    }
-    async with ClientSession(headers=headers) as session:
+    async with ClientSession(headers=headers_brawl_api) as session_brawl_api, ClientSession(
+            headers=headers_brawlify) as session_brawlify:
         from app import dp
-        Sessions(session=session, name="brawl_api")
+        Sessions(session=session_brawl_api, name="brawl_api")
+        Sessions(session=session_brawlify, name="brawlify")
         await create_table()
         await init()
 
