ca1804dfbfe65818323f744b68a77cf500b43b18
anylevel <vadyusha.surin@gmail.com>
2022-08-21 13:59:37 +0300

    add command daily meta upcoming and refactoring command daily meta

--- app/handlers/brawlify/brawlify.py
+++ app/handlers/brawlify/brawlify.py
@@ -12,11 +12,23 @@ async def get_daily_meta(message: types.Message):
     url_get_events = "https://brawlify.com/#"
     async with Sessions.get_response(name=session_name_brawlify, url=url_get_events) as response_events:
         data_events = await response_events.read()
-    buttons = await parse_daily_meta(data_events=data_events)
-    keyboard = types.InlineKeyboardMarkup(row_width=1)
-    keyboard.add(*buttons)
-    await message.answer('Choose a mod to watch the winrate of brawlers:', reply_markup=keyboard)
+    buttons_current_events = await parse_daily_meta(data_events=data_events,
+                                                    class_='container-fluid post-type4 pt-1 pb-3')
+    keyboard_current_events = types.InlineKeyboardMarkup(row_width=1)
+    keyboard_current_events.add(*buttons_current_events)
+    await message.answer('Choose a mod to watch the win rate of brawlers:', reply_markup=keyboard_current_events)
 
+
+@dp.message_handler(commands=['daily_meta_upcoming'])
+async def get_daily_meta_upcoming(message: types.Message):
+    url_get_events = "https://brawlify.com/#"
+    async with Sessions.get_response(name=session_name_brawlify, url=url_get_events) as response_events:
+        data_events = await response_events.read()
+    buttons_upcoming_events = await parse_daily_meta(data_events=data_events, class_='container-fluid pb-2 post-type1')
+    keyboard_upcoming_events = types.InlineKeyboardMarkup(row_width=1)
+    keyboard_upcoming_events.add(*buttons_upcoming_events)
+    await message.answer('Choose upcoming events to watch the win rate of brawler:',
+                         reply_markup=keyboard_upcoming_events)
 
 
 @dp.callback_query_handler(lambda c: "Win rate" in c.data)
