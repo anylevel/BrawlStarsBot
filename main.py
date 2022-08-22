0d21e40245123d1b10aa2659e3a2279bf0d7b126
anylevel <vadyusha.surin@gmail.com>
2022-08-22 12:23:23 +0300

    import + create task shedulers

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from app.sessions import Sessions
 from app.database import create_table, init
 from app.middlewares import ThrottlingMiddleware, TokenMiddleware
 from app.constans import get_token_bot, headers_brawl_api, headers_brawlify
+from app.models import Player, Club
 import asyncio
 
 bot = Bot(token=get_token_bot())
@@ -20,7 +21,8 @@ async def main():
     async with ClientSession(headers=headers_brawl_api) as session_brawl_api, ClientSession(
             headers=headers_brawlify) as session_brawlify:
         from app import dp
-        from app.shedulers.brawl_battlelog import battle_log_update_info
+        from app.shedulers.brawl_battlelog import brawl_schedule
+        from app.shedulers.utils import change_battle_log, update_player, update_club
         Sessions(session=session_brawl_api, name="brawl_api")
         Sessions(session=session_brawlify, name="brawlify")
         await create_table()
@@ -28,7 +30,10 @@ async def main():
 
         dp.middleware.setup(ThrottlingMiddleware())
         dp.middleware.setup(TokenMiddleware())
-        asyncio.create_task(battle_log_update_info())
+        asyncio.create_task(
+            brawl_schedule(time=100, model=Player, func=change_battle_log, url_model='players', endpoint='battlelog'))
+        asyncio.create_task(brawl_schedule(time=100, model=Player, func=update_player, url_model='players'))
+        asyncio.create_task(brawl_schedule(time=100, model=Club, func=update_club, url_model='clubs'))
         await dp.start_polling()
 
 
