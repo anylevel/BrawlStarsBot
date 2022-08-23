88726d5aceacf3c045d2b58ba38a6b133e0c030c
anylevel <vadyusha.surin@gmail.com>
2022-08-23 12:12:25 +0300

    add logger

--- main.py
+++ main.py
@@ -6,17 +6,11 @@ from app.database import create_table, init
 from app.middlewares import ThrottlingMiddleware, TokenMiddleware
 from app.constans import get_token_bot, headers_brawl_api, headers_brawlify
 from app.models import Player, Club
-from aiologger import Logger
-from aiologger.handlers.files import AsyncFileHandler
+from app.loggers import Loggers
 import asyncio
-import datetime
 
 bot = Bot(token=get_token_bot())
 dp = Dispatcher(bot=bot, storage=MemoryStorage())
-# TODO refactoring
-handler = AsyncFileHandler(filename="logger.txt")
-logger = Logger.with_default_handlers()
-logger.add_handler(handler=handler)
 
 
 # TODO Разобраться в локалях языка и добавить их
@@ -33,13 +27,14 @@ async def main():
         Sessions(session=session_brawlify, name="brawlify")
         await create_table()
         await init()
+        logger = Loggers(filename='log_main.txt', name='log_main')
         dp.middleware.setup(ThrottlingMiddleware())
         dp.middleware.setup(TokenMiddleware())
         asyncio.create_task(
             brawl_schedule(time=10000, model=Player, func=change_battle_log, url_model='players', endpoint='battlelog'))
         asyncio.create_task(brawl_schedule(time=10000, model=Player, func=update_player, url_model='players'))
         asyncio.create_task(brawl_schedule(time=10000, model=Club, func=update_club, url_model='clubs'))
-        await logger.info(f"Start polling:{datetime.datetime.now()}")
+        await logger.info("Start polling")
         await dp.start_polling()
 
 
